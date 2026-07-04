9c1f151773d52e1e7dfe71f7223341bcbecac80d
ahuer <18221079843@139.com>
2026-07-04 12:18:58 +0800

    fix example

--- cyber/examples/common_component_example/common_component_example.cc
+++ cyber/examples/common_component_example/common_component_example.cc
@@ -20,9 +20,7 @@ bool CommonComponentSample::Init() {
   return true;
 }
 
-bool CommonComponentSample::Proc(const std::shared_ptr<Driver>& msg0,
-                                 const std::shared_ptr<Driver>& msg1) {
-  AINFO << "Start common component Proc [" << msg0->msg_id() << "] ["
-        << msg1->msg_id() << "]";
+bool CommonComponentSample::Proc(const std::shared_ptr<Driver>& msg) {
+  AINFO << "receive msg id:  " <<  msg->msg_id() ;
   return true;
 }
--- cyber/examples/timer_component_example/timer_component_example.cc
+++ cyber/examples/timer_component_example/timer_component_example.cc
@@ -28,10 +28,8 @@ bool TimerComponentSample::Init() {
 bool TimerComponentSample::Proc() {
   static int i = 0;
   auto out_msg = std::make_shared<Driver>();
-  out_msg->set_content("hello apollo.");
   out_msg->set_msg_id(i++);
   driver_writer_->Write(out_msg);
-  AINFO << "timer_component_example: Write drivermsg->"
-        << out_msg->ShortDebugString();
+  AINFO << "send msg id: " << out_msg->msg_id();
   return true;
 }
